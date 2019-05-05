8100dc82d2dddca561405616d995d5c154434943
KuznetsovMA <jztenk@gmail.com>
2019-05-05 15:23:53 +0300

    mainPinClickHanlder

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import {getAdsData} from './ads';
-import {createPinElements} from './pins';
+import {createPinElements, renderPinElements} from './pins';
 import {createCardElements} from './cards';
 
 const MAX_ADS = 8;
@@ -7,8 +7,7 @@ const MAX_ADS = 8;
 const mapBlock = document.querySelector(`.map`);
 const pinsBlock = mapBlock.querySelector(`.map__pins`);
 const filtersBlock = mapBlock.querySelector(`.map__filters-container`);
-
-// Enabling page
+const mainPin = mapBlock.querySelector(`.map__pin--main`);
 
 // Creating ads mock data
 const adsData = getAdsData(MAX_ADS);
@@ -19,8 +18,12 @@ const pinElements = createPinElements(adsData);
 // Creating card elements
 const cardElements = createCardElements(adsData);
 
-// Inserting pin elements
-pinsBlock.appendChild(pinElements);
+// Enabling page & inserting pin elements
+const mainPinClickHandler = () => {
+  mapBlock.classList.remove(`map--faded`);
+  renderPinElements(pinsBlock, pinElements);
+};
+mainPin.addEventListener(`click`, mainPinClickHandler);
 
 // Inserting card elements
 mapBlock.insertBefore(cardElements, filtersBlock);
